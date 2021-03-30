b76b0079dca97e6c00665b8eabb21cf87ebf690f
Latte <cchukwuani@gmail.com>
2021-03-30 01:40:00 -0400

    added to my models index

--- Main/models/comment.js
+++ Main/models/comment.js
@@ -1,45 +1,49 @@
-const { Model, Datatypes} = require('sequelize');
+const { Model, Datatypes } = require('sequelize');
 const sequelize = require('../config/connection');
 
 class Comment extends Model {}
 
 Comment.init(
-    {
-        id: {
-            type: Datatypes.INTEGER,
-            primaryKey: true,
-            allowNull: false,
-            autoIncrement: true
-        },
-        comment_text: {
-            type: Datatypes.STRING,
-            allowNull: false,
-            validate: {
-            }
-        },
-        user_id: {
-            type: Datatypes.INTEGER,
-            allowNull: false,
-            references: {
-                model: 'user',
-                key: 'id'
-            }
-        },
-        post_id: {
-            type: Datatypes.INTEGER,
-            allowNull: false,
-            references: {
-                model: 'blogPost',
-                key: 'id'
-            }
-        }
+  {
+    id: {
+      type: Datatypes.INTEGER,
+      primaryKey: true,
+      allowNull: false,
+      autoIncrement: true,
     },
-    {
-        sequelize,
-        freezeTableName: true,
-        underscored: true,
-        modelName: 'comment'
-    }
+    comment_text: {
+      type: Datatypes.STRING,
+      allowNull: false,
+      validate: {},
+    },
+    date_created: {
+      type: DataTypes.DATE,
+      allowNull: false,
+      defaultValue: DataTypes.NOW,
+    },
+    user_id: {
+      type: Datatypes.INTEGER,
+      allowNull: false,
+      references: {
+        model: 'user',
+        key: 'id',
+      },
+    },
+    post_id: {
+      type: Datatypes.INTEGER,
+      allowNull: false,
+      references: {
+        model: 'blogPost',
+        key: 'id',
+      },
+    },
+  },
+  {
+    sequelize,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'comment',
+  }
 );
 
 module.exports = Comment;
--- Main/models/index.js
+++ Main/models/index.js
@@ -1,13 +1,31 @@
 const User = require('./User');
 const blogPost = require('./blogPost');
+const Comment = require('./comment');
 
+// User can have many Blog Post
 User.hasMany(blogPost, {
   foreignKey: 'user_id',
-  onDelete: 'CASCADE'
+  onDelete: 'CASCADE',
 });
-
+// User has many commments
+User.hasMany(Comment, {
+  foreignKey: 'user_id',
+});
+// Blog post belongs to a User => cannot exist without a User
 blogPost.belongsTo(User, {
-  foreignKey: 'user_id'
+  foreignKey: 'user_id',
+});
+// There can be multiple comments per blog post
+blogPost.hasMany(Comment, {
+  foreignKey: 'post_id',
+});
+// Comment belongs to a User => cannot exist without a User
+Comment.belongsTo(User, {
+  foreignKey: 'user_id',
+});
+// Comment belongs to a blog post => cannot exist without a Blog Post
+Comment.belongsTo(blogPost, {
+  foreignKey: 'post_id',
 });
 
 module.exports = { User, blogPost };
